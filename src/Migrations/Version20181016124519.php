025013dbb9171268eca80b4ce4d1595d73e374c6
Nils Wilmet <n.wilmet@agiirnetwork.com>
2018-10-16 14:55:02 +0200

    [FIX] Woking migrations files

--- src/Migrations/Version20181016124519.php
+++ src/Migrations/Version20181016124519.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181015200800 extends AbstractMigration
+final class Version20181016124519 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
