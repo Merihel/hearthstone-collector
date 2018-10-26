0ba5be109c9cacc8ccf663d420ab554091624ce7
Nils Wilmet <n.wilmet@agiirnetwork.com>
2018-10-26 21:16:50 +0200

    [TASKS] Added more entities, relations, and a documentation at v0.1
    Ajout de la relation entre Users et Cards, et entre Users et Decks. On peut maintenant assigner des cartes à un user, et des decks à un user, et récupérer ces données par user.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,43 @@
 
 namespace App\Controller;
 
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class UserController extends AbstractController
-{
+{   
+    /**
+     * @Route("/user/{id}")
+     */
+    public function getUserAction($id, Container $container)
+    {
+        //Container possède les services, not celui de JMS
+        //Je créé un objet JMSSerializer pour la sérialisation/déserialisation
+        $serializer = $container->get('jms_serializer');
+        
+        //Je déclare un objet User que je récupère grâce au manager de Doctrine, qui utilise le repository de mon User
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($id);
+        
+        //Si je n'ai pas d'user, je lève une exception
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        } else {
+            //Sinon je créé mon objet JSON via la fonction "serialize" de JMS, et l'envoie en front
+            $jsonObject = $serializer->serialize($user, 'json');
+            return $this->json($jsonObject);
+        }
+    }
+    
     /**
      * @Route("/user", name="user")
      */
@@ -24,25 +53,85 @@ class UserController extends AbstractController
     /**
      * @Route("/new-user")
      */
-    public function newUserAction(Request $request)
+    public function newUserAction(Request $request, Container $container)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $serializer = $container->get('jms_serializer');
+        //Deserialize json from HTTP POST into a valid User object
+        $user = $serializer->deserialize($request->request->get('json'), 'App\Entity\User', 'json');
+        
+        // tell Doctrine you want to (eventually) save the User (no queries yet)
+        $em->persist($user);
+
+        try {
+            // actually executes the queries (i.e. the INSERT query)
+            $em->flush();
+            
+            return $this->json([
+                'state' => 'SUCCESS',
+                'message' => 'Utilisateur enregistré',
+                'id' => $user->getId(),
+            ]);
+        } catch (Exception $e) {
+            return $this->json([
+                'state' => 'ERROR',
+                'message' => 'Erreur lors de l\'enregistrement de l\'utilisateur',
+                'id' => $user->getId(),
+                'devMessage' => $e->getMessage(),
+            ]);
+        }
+    }
+    
+    /**
+     * @Route("/update-user")
+     */
+    public function updateUserAction(Request $request, Container $container)
     {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-        $user = new User();
-        $user->setPseudo($request->request->get('pseudo'));
-        $user->setMail($request->request->get('mail'));
-        $user->setCoins($request->request->get('coins'));
+        $em = $this->getDoctrine()->getManager();
+        $serializer = $container->get('jms_serializer');
+        
+        try {
+            //Deserialize json from HTTP POST into a valid User object
+            $user = $serializer->deserialize($request->request->get('json'), 'App\Entity\User', 'json');
 
-        // tell Doctrine you want to (eventually) save the User (no queries yet)
-        $entityManager->persist($user);
+        } catch (\JMS\Serializer\Exception\RuntimeException $e) {
+            return $this->json([
+                'state' => 'ERROR',
+                'message' => 'Erreur lors de l\'envoi des données',
+                'devMessage' => 'Error deserializing JSON: '.$request->request->get('json'),
+            ]);
+        }
+        
+        try {
+            // tell Doctrine you want to (eventually) update the User (no queries yet)
+            $em->merge($user);
+        } catch (\Doctrine\ORM\EntityNotFoundException $e) {
+            return $this->json([
+                'state' => 'ERROR',
+                'message' => 'Utilisateur non trouvé',
+                'devMessage' => 'Error updating user with id '.$user->getId().': user not found'
+            ]);
+        }
+        
+        try {
+            // actually executes the queries (i.e. the INSERT query)
+            $em->flush();
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+            return $this->json([
+                'state' => 'SUCCESS',
+                'message' => 'Utilisateur mis à jour !',
+                'id' => $user->getId(),
+            ]);
+        } catch (\Doctrine\ORM\ORMException $e) {
+            return $this->json([
+                'state' => 'ERROR',
+                'message' => 'Erreur lors de la mise à jour',
+                'devMessage' => 'Error updating user with id '.$user->getId().': databse update error'
+            ]);
+        }
 
-        return $this->json([
-            'message' => 'Successfully saved user',
-            'id' => $user->getId(),
-        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -40,6 +42,50 @@ class User
      * @ORM\Column(type="integer")
      */
     private $coins;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Card")
+     * @ORM\JoinTable(name="users_cards",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="card_id", referencedColumnName="id")}
+     *      )
+     */
+    private $cards;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Deck")
+     * @ORM\JoinTable(name="users_decks",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="deck_id", referencedColumnName="id")}
+     *      )
+     */
+    private $decks;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Card")
+     * @ORM\JoinTable(name="wishlist",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="card_wanted_id", referencedColumnName="id")}
+     *      )
+     */
+    private $wishlist;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $facebookId;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $googleId;
+
+    public function __construct()
+    {
+        $this->cards = new ArrayCollection();
+        $this->decks = new ArrayCollection();
+        $this->wishlist = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -102,6 +148,108 @@ class User
     public function setCoins(int $coins): self
     {
         $this->coins = $coins;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Card[]
+     */
+    public function getCards(): Collection
+    {
+        return $this->cards;
+    }
+
+    public function addCard(Card $card): self
+    {
+        if (!$this->cards->contains($card)) {
+            $this->cards[] = $card;
+        }
+
+        return $this;
+    }
+
+    public function removeCard(Card $card): self
+    {
+        if ($this->cards->contains($card)) {
+            $this->cards->removeElement($card);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Deck[]
+     */
+    public function getDecks(): Collection
+    {
+        return $this->decks;
+    }
+
+    public function addDeck(Deck $deck): self
+    {
+        if (!$this->decks->contains($deck)) {
+            $this->decks[] = $deck;
+        }
+
+        return $this;
+    }
+
+    public function removeDeck(Deck $deck): self
+    {
+        if ($this->decks->contains($deck)) {
+            $this->decks->removeElement($deck);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Card[]
+     */
+    public function getWishlist(): Collection
+    {
+        return $this->wishlist;
+    }
+
+    public function addWishlist(Card $wishlist): self
+    {
+        if (!$this->wishlist->contains($wishlist)) {
+            $this->wishlist[] = $wishlist;
+        }
+
+        return $this;
+    }
+
+    public function removeWishlist(Card $wishlist): self
+    {
+        if ($this->wishlist->contains($wishlist)) {
+            $this->wishlist->removeElement($wishlist);
+        }
+
+        return $this;
+    }
+
+    public function getFacebookId(): ?string
+    {
+        return $this->facebookId;
+    }
+
+    public function setFacebookId(?string $facebookId): self
+    {
+        $this->facebookId = $facebookId;
+
+        return $this;
+    }
+
+    public function getGoogleId(): ?string
+    {
+        return $this->googleId;
+    }
+
+    public function setGoogleId(?string $googleId): self
+    {
+        $this->googleId = $googleId;
 
         return $this;
     }
